64de30118bdfa69caff2363d1df9dafce813d691
Mg-dev <tyt22yearsold@gmail.com>
2023-02-02 01:24:01 +0630

    stop watch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+  const timerRef = useRef(0);
+
+  const startHandler = () => {
+    if(timerRef.current) return;
+    timerRef.current = setInterval(()=> setValue(c=>c+1), 1000);
+    console.log(timerRef.current)
+  }
+
+  const stopHandler = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = 0;
+  }
+
+  const resetHandler = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = 0;
+    setValue(0);
+  }
+
+  useEffect(()=>{
+    return () => clearInterval(timerRef.current)
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stop Watch</h1>
+      <h3>{value}</h3>
+      <div className="btn-container">
+        <button onClick={startHandler}>start</button>
+        <button onClick={stopHandler}>stop</button>
+        <button onClick={resetHandler}>reset</button>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
+
